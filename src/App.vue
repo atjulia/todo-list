b913e93eb071e8de8029ab19f597d7d2f300506c
Julia Karolina <juliakarolina24@gmail.com>
2022-06-15 15:09:28 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+	<div id="app">
+		<h1>Tarefas</h1>
+	</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+	body {
+		font-family: 'Inter', sans-serif;
+		background: #f6f6f6;
+		color: #494949;
+	}
+
+	#app {
+		display: flex;
+		flex: 1;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		height: 100vh;
+	}
+
+	#app h1 {
+		margin-bottom: 5px;
+		font-weight: 300;
+		font-size: 3rem;
+	}
+</style>
